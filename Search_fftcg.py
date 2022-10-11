90707c2194e79f47baa21fee7b0cf345d545e494
TC <dche211@mywhitecliffe.com>
2022-10-12 11:26:59 +1300

    Can choose to find through card code or name.

--- Search_fftcg.py
+++ Search_fftcg.py
@@ -2,17 +2,32 @@ import openpyxl
 import os
 from path import RootPath
 
-def find_specific_cell():
+def find_cardName(currentSheet, Card_Name):
+    for row in range(1, currentSheet.max_row + 1):
+        for column in range(1,703): #columns
+            cell1Column = num_hash(column)
+            cell2Column = num_hash(column - 1)
+            cell3Column = num_hash(column + 1)
+            cell_name = "{}{}".format(cell1Column, row)
+            cell_name2 = "{}{}".format(cell2Column, row)
+            cell_name3 = "{}{}".format(cell3Column, row)
+            if currentSheet[cell_name].value == Card_Name: #Can't manipulate the cell value so can't use upper(), title() etc...
+                print("There are {} {} {}. It is at cell {}".format(currentSheet[cell_name3].value, currentSheet[cell_name].value, currentSheet[cell_name2].value ,cell_name))
+
+def find_cardCode(currentSheet, Card_Code):
     for row in range(1, currentSheet.max_row + 1):
         for column in range(1,703): #columns
             cell1Column = num_hash(column)
             cell2Column = num_hash(column + 1)
+            cell3Column = num_hash(column + 2)
             cell_name = "{}{}".format(cell1Column, row)
             cell_name2 = "{}{}".format(cell2Column, row)
-            if currentSheet[cell_name].value == Card_Code: #Case sensitive
-                print("{} {} is at {}".format(currentSheet[cell_name2].value, currentSheet[cell_name].value, cell_name))
+            cell_name3 = "{}{}".format(cell3Column, row)
+            if currentSheet[cell_name].value == Card_Code: #Can't manipulate the cell value so can't use upper(), title() etc...
+                print("There are {} {} {}. It is at cell {}".format(currentSheet[cell_name3].value, currentSheet[cell_name].value, currentSheet[cell_name2].value ,cell_name))
 
 def num_hash(num):
+    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     if num < 26:
         return alpha[num-1]
     else:
@@ -25,22 +40,46 @@ def num_hash(num):
         else:
             return num_hash(quotient) + alpha[remainder-1]
 
-#Variables
+def find_cardBYname():
+    Card_Name = input("Enter the name of the card you wish to find: ").title() # Search result is Case sensitive
+
+    for sheet in SheetNames:
+        print("\nCurrent sheet is: {}".format(sheet) + "\n")
+        currentSheet = File[sheet]
+        find_cardName(currentSheet, Card_Name)
+
+def find_cardBYcode():
+    Card_Code = input("Enter the name of the card you wish to find: ").title() # Search result is Case sensitive
+
+    for sheet in SheetNames:
+        print("\nCurrent sheet is: {}".format(sheet) + "\n")
+        currentSheet = File[sheet]
+        find_cardCode(currentSheet, Card_Code)
+
+def inputChoice():
+    codeORname = input("Press \"C\" if you want to find card by its code or \"N\" if you want to search by its name: ").upper()
+    if (codeORname == "C"):
+        find_cardBYcode()
+    if (codeORname == "N"):
+        find_cardBYname()
+    else:
+        print("Not valid input")
+
+#Variables / Entry
 relativePath = os.path.join(RootPath, "FFTCG.xlsx")
 File = openpyxl.load_workbook(relativePath, data_only=True)
 SheetNames = File.sheetnames
-Card_Code = input("Enter the code of the card you wish to find: ").title() #Is not effective with card names
-alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+print("Welcome to the FFTCG spreadsheet searcher!\n")
+inputChoice()
 
-#Entry point
-for sheet in SheetNames:
-    print("\nCurrent sheet is: {}".format(sheet) + "\n")
-    currentSheet = File[sheet]
-    find_specific_cell()
+choice = True
+while True:
+    choice = input("Do you want to find another card? (Y = yes, N = no)\n").upper()
+    if (choice == "Y"):
+        choice == True
+        inputChoice()
 
-choice = input("Do you want to find another card? (Y = yes, N = no)")
-
-while choice == "Y":
-    Card_Code
-else:
-    print("Thank you for using this python script!")
+    if (choice == "N"):
+        choice == False
+        print("Thank you for using this Python script!")
+        break
