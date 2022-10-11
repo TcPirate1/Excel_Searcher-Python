effdfd77b933c4c883f73b1c9fd358ef3122266e
TC <dche211@mywhitecliffe.com>
2022-10-11 23:44:10 +1300

    Replace capitalize with upper because of how excel's proper function works

--- Search_fftcg.py
+++ Search_fftcg.py
@@ -7,7 +7,7 @@ def find_specific_cell():
         for column in range(1,703): #Rows
             column = num_hash(column)
             cell_name = "{}{}".format(column, row)
-            if currentSheet[cell_name.capitalize()].value == Card_Name: #Case sensitive
+            if currentSheet[cell_name.upper()].value == Card_Name: #Case sensitive
                 print("{} is at {}".format(currentSheet[cell_name].value, cell_name))
 
 def num_hash(num):
@@ -27,7 +27,7 @@ def num_hash(num):
 relativePath = os.path.join(RootPath, "FFTCG.xlsx")
 File = openpyxl.load_workbook(relativePath, data_only=True)
 SheetNames = File.sheetnames
-Card_Name = input("Enter the code of the card you wish to find: ").capitalize()
+Card_Name = input("Enter the code of the card you wish to find: ").upper()
 alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 #Entry point
