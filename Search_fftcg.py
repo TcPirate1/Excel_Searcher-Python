dcd4795cd82e546ad8a851c5201ab650094cad07
TC <88120195+TcPirate1@users.noreply.github.com>
2022-10-13 16:55:00 +1300

    Seperated Search_fftcg.py into multiple files and still works as the same

--- Search_fftcg.py
+++ Search_fftcg.py
@@ -1,60 +1,4 @@
-import openpyxl
-import os
-from path import RootPath
-
-def find_cardName(currentSheet, Card_Name):
-    for row in range(1, currentSheet.max_row + 1):
-        for column in range(1,703): #columns
-            cell1Column = num_hash(column)
-            cell2Column = num_hash(column - 1)
-            cell3Column = num_hash(column + 1)
-            cell_name = "{}{}".format(cell1Column, row)
-            cell_name2 = "{}{}".format(cell2Column, row)
-            cell_name3 = "{}{}".format(cell3Column, row)
-            if currentSheet[cell_name].value == Card_Name: #Can't manipulate the cell value so can't use upper(), title() etc...
-                print("There are {} {} {}. It is at cell {}".format(currentSheet[cell_name3].value, currentSheet[cell_name].value, currentSheet[cell_name2].value ,cell_name))
-
-def find_cardCode(currentSheet, Card_Code):
-    for row in range(1, currentSheet.max_row + 1):
-        for column in range(1,703): #columns
-            cell1Column = num_hash(column)
-            cell2Column = num_hash(column + 1)
-            cell3Column = num_hash(column + 2)
-            cell_name = "{}{}".format(cell1Column, row)
-            cell_name2 = "{}{}".format(cell2Column, row)
-            cell_name3 = "{}{}".format(cell3Column, row)
-            if currentSheet[cell_name].value == Card_Code: #Can't manipulate the cell value so can't use upper(), title() etc...
-                print("There are {} {} {}. It is at cell {}".format(currentSheet[cell_name3].value, currentSheet[cell_name].value, currentSheet[cell_name2].value ,cell_name))
-
-def num_hash(num):
-    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    if num < 26:
-        return alpha[num-1]
-    else:
-        quotient, remainder = num // 26, num % 26
-        if remainder == 0:
-            if quotient == 1:
-                return alpha[remainder-1]
-            else:
-                return num_hash(quotient-1) + alpha[remainder-1]
-        else:
-            return num_hash(quotient) + alpha[remainder-1]
-
-def find_cardBYname():
-    Card_Name = input("Enter the name of the card you wish to find: ").title() # Search result is Case sensitive
-
-    for sheet in SheetNames:
-        print("\nCurrent sheet is: {}".format(sheet) + "\n")
-        currentSheet = File[sheet]
-        find_cardName(currentSheet, Card_Name)
-
-def find_cardBYcode():
-    Card_Code = input("Enter the name of the card you wish to find: ").title() # Search result is Case sensitive
-
-    for sheet in SheetNames:
-        print("\nCurrent sheet is: {}".format(sheet) + "\n")
-        currentSheet = File[sheet]
-        find_cardCode(currentSheet, Card_Code)
+from Find_Card import find_cardBYcode, find_cardBYname
 
 def inputChoice():
     codeORname = input("Press \"C\" if you want to find card by its code or \"N\" if you want to search by its name: ").upper()
@@ -62,13 +6,8 @@ def inputChoice():
         find_cardBYcode()
     if (codeORname == "N"):
         find_cardBYname()
-    if (codeORname != "Y" or "N"):
-        print("Not valid input")
 
 #Variables / Entry
-relativePath = os.path.join(RootPath, "FFTCG.xlsx")
-File = openpyxl.load_workbook(relativePath, data_only=True)
-SheetNames = File.sheetnames
 print("Welcome to the FFTCG spreadsheet searcher!\n")
 inputChoice()
 
