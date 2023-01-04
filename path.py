d87012ef18aa5fd23d4c8784353e7f47469a178c
Terence Chen <88120195+TcPirate1@users.noreply.github.com>
2023-01-05 10:35:36 +1300

    Refactor (#4)
    * Changed path.py variable name(s) and refactored Search_fftcg.py and changed title() to upper() in Find_Card.py
    
    * Added DisplayCurrentSheet function

--- path.py
+++ path.py
@@ -1,6 +1,6 @@
 import openpyxl
 import os
 
-relativePath = os.path.join(os.environ['USERPROFILE'], 'Desktop', 'FFTCG.xlsx')
-File = openpyxl.load_workbook(relativePath, data_only=True)
+Path = os.path.join(os.environ['USERPROFILE'], 'Documents', 'Spreadsheets', 'FFTCG.xlsx')
+File = openpyxl.load_workbook(Path, data_only=True)
 SheetNames = File.sheetnames
