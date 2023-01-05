e2f539dcf541c9ec7cc73899c2a5fe3a1a5e89c8
Terence Chen <88120195+TcPirate1@users.noreply.github.com>
2023-01-05 13:15:31 +1300

    gets the path for user's home directory irrespective of OS. (#5)
    uses os.path.expandeduser()

--- path.py
+++ path.py
@@ -1,6 +1,6 @@
 import openpyxl
 import os
 
-Path = os.path.join(os.environ['USERPROFILE'], 'Documents', 'Spreadsheets', 'FFTCG.xlsx')
+Path = os.path.join(os.path.expanduser('~'), 'Documents', 'Spreadsheets', 'FFTCG.xlsx')
 File = openpyxl.load_workbook(Path, data_only=True)
 SheetNames = File.sheetnames
