5b7ecea6b365f8bb14c5c25c8a7eb08c91e86193
TC_linux <88120195+TcPirate1@users.noreply.github.com>
2023-01-05 13:09:17 +1300

    Implemented Regex which allowed for only 1 function instead of 2 functions with 99% duplicity.

--- Find_Card.py
+++ Find_Card.py
@@ -1,5 +1,6 @@
 from ColumnConverter import num_hash
 from path import SheetNames, File
+import re
 
 def find_card(codeORname):
     if (codeORname == "N"):
@@ -7,40 +8,35 @@ def find_card(codeORname):
 
         for sheet in SheetNames:
             currentSheet = DisplayCurrentSheet(sheet)
-            find_cardName(currentSheet, Card_Name)
+            find_cardLocation(currentSheet, Card_Name)
 
     if (codeORname == "C"):
-        Card_Code = input("Enter the code of the card you wish to find: ").upper() # Search result is Case sensitive
+        Card_Name = input("Enter the code of the card you wish to find: ").upper() # Search result is Case sensitive
 
         for sheet in SheetNames:
             currentSheet = DisplayCurrentSheet(sheet)
-            find_cardCode(currentSheet, Card_Code)
+            find_cardLocation(currentSheet, Card_Name)
 
 def DisplayCurrentSheet(sheet):
     print("\nCurrent sheet is: {}".format(sheet) + "\n")
     currentSheet = File[sheet]
     return currentSheet
 
-def find_cardName(currentSheet, Card_Name):
+def find_cardLocation(currentSheet, Card_Name):
+    cardNameRegex = re.match('^\d{1,2}-\d{3}[CRHLS]+$', Card_Name)
     for row in range(1, currentSheet.max_row + 1):
         for column in range(1,currentSheet.max_column + 1): #columns
             cell1Column = num_hash(column)
             cell2Column = num_hash(column - 1)
             cell3Column = num_hash(column + 1)
-            cell_name = "{}{}".format(cell1Column, row)
-            card_code_cell = "{}{}".format(cell2Column, row)
-            card_quantity_cell = "{}{}".format(cell3Column, row)
-            if currentSheet[cell_name].value == Card_Name: #Can't manipulate the cell value so can't use upper(), title() etc...
-                print("There are {} {} {}. It is at cell {}".format(currentSheet[card_quantity_cell].value, currentSheet[cell_name].value, currentSheet[card_code_cell].value ,cell_name))
+            cell4Column = num_hash(column + 2)
+            searchTarget = "{}{}".format(cell1Column, row)
+            left_cell = "{}{}".format(cell2Column, row)
+            right_cell = "{}{}".format(cell3Column, row)
+            right_2Cells = "{}{}".format(cell4Column, row)
 
-def find_cardCode(currentSheet, Card_Code):
-    for row in range(1, currentSheet.max_row + 1):
-        for column in range(1, currentSheet.max_column + 1): #columns
-            cell1Column = num_hash(column)
-            cell2Column = num_hash(column + 1)
-            cell3Column = num_hash(column + 2)
-            cell_name = "{}{}".format(cell1Column, row)
-            card_code_cell = "{}{}".format(cell2Column, row)
-            card_quantity_cell = "{}{}".format(cell3Column, row)
-            if currentSheet[cell_name].value == Card_Code: #Can't manipulate the cell value so can't use upper(), title() etc...
-                print("There are {} {} {}. It is at cell {}".format(currentSheet[card_quantity_cell].value, currentSheet[cell_name].value, currentSheet[card_code_cell].value ,cell_name))
+            if (currentSheet[searchTarget].value == Card_Name and cardNameRegex == None): #Can't manipulate the cell value so can't use upper(), title() etc...
+                print(f"There are {currentSheet[right_cell].value} {currentSheet[searchTarget].value} {currentSheet[left_cell].value}. It is at cell {searchTarget}")
+
+            if (currentSheet[searchTarget].value == Card_Name and cardNameRegex != None): #re.match returns None if no matches are found
+                print(f"There are {currentSheet[right_2Cells].value} {currentSheet[searchTarget].value} {currentSheet[right_cell].value}. It is at cell {searchTarget}")
