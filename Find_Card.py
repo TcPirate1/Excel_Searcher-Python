0a80aeaa6cd07dd09e55886d2cd818bfd966a094
TC_linux <88120195+TcPirate1@users.noreply.github.com>
2023-01-05 13:20:03 +1300

    Changed variable name Card_Name to Card

--- Find_Card.py
+++ Find_Card.py
@@ -4,26 +4,26 @@ import re
 
 def find_card(codeORname):
     if (codeORname == "N"):
-        Card_Name = input("Enter the name of the card you wish to find: ").upper() # Search result is Case sensitive 
+        Card = input("Enter the name of the card you wish to find: ").upper() # Search result is Case sensitive 
 
         for sheet in SheetNames:
             currentSheet = DisplayCurrentSheet(sheet)
-            find_cardLocation(currentSheet, Card_Name)
+            find_cardLocation(currentSheet, Card)
 
     if (codeORname == "C"):
-        Card_Name = input("Enter the code of the card you wish to find: ").upper() # Search result is Case sensitive
+        Card = input("Enter the code of the card you wish to find: ").upper() # Search result is Case sensitive
 
         for sheet in SheetNames:
             currentSheet = DisplayCurrentSheet(sheet)
-            find_cardLocation(currentSheet, Card_Name)
+            find_cardLocation(currentSheet, Card)
 
 def DisplayCurrentSheet(sheet):
     print("\nCurrent sheet is: {}".format(sheet) + "\n")
     currentSheet = File[sheet]
     return currentSheet
 
-def find_cardLocation(currentSheet, Card_Name):
-    cardNameRegex = re.match('^\d{1,2}-\d{3}[CRHLS]+$', Card_Name)
+def find_cardLocation(currentSheet, Card):
+    cardNameRegex = re.match('^\d{1,2}-\d{3}[CRHLS]+$', Card)
     for row in range(1, currentSheet.max_row + 1):
         for column in range(1,currentSheet.max_column + 1): #columns
             cell1Column = num_hash(column)
@@ -35,8 +35,8 @@ def find_cardLocation(currentSheet, Card_Name):
             right_cell = "{}{}".format(cell3Column, row)
             right_2Cells = "{}{}".format(cell4Column, row)
 
-            if (currentSheet[searchTarget].value == Card_Name and cardNameRegex == None): #Can't manipulate the cell value so can't use upper(), title() etc...
+            if (currentSheet[searchTarget].value == Card and cardNameRegex == None): #Can't manipulate the cell value so can't use upper(), title() etc...
                 print(f"There are {currentSheet[right_cell].value} {currentSheet[searchTarget].value} {currentSheet[left_cell].value}. It is at cell {searchTarget}")
 
-            if (currentSheet[searchTarget].value == Card_Name and cardNameRegex != None): #re.match returns None if no matches are found
+            if (currentSheet[searchTarget].value == Card and cardNameRegex != None): #re.match returns None if no matches are found
                 print(f"There are {currentSheet[right_2Cells].value} {currentSheet[searchTarget].value} {currentSheet[right_cell].value}. It is at cell {searchTarget}")
