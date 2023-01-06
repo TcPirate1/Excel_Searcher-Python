f59083a24fb56b8f0257941c0af63982b702693d
Terence Chen <88120195+TcPirate1@users.noreply.github.com>
2023-01-06 17:39:42 +1300

    Further refactoring of code (#7)
    Created a new file to house the find_cardLocation function.
    Changed some text and added YES and NO options to the doAgain function  in Search_fftcg.py.

--- Find_Card.py
+++ Find_Card.py
@@ -1,6 +1,5 @@
-from ColumnConverter import num_hash
 from path import SheetNames, File
-import re
+from card_location import find_cardLocation
 
 def find_card(codeORname):
     if (codeORname == "N"):
@@ -18,25 +17,6 @@ def find_card(codeORname):
             find_cardLocation(currentSheet, Card)
 
 def DisplayCurrentSheet(sheet):
-    print("\nCurrent sheet is: {}".format(sheet) + "\n")
+    print(f"\nCurrent sheet is: {sheet}\n")
     currentSheet = File[sheet]
     return currentSheet
-
-def find_cardLocation(currentSheet, Card):
-    cardNameRegex = re.match('^\d{1,2}-\d{3}[CRHLS]+$', Card)
-    for row in range(1, currentSheet.max_row + 1):
-        for column in range(1,currentSheet.max_column + 1): #columns
-            cell1Column = num_hash(column)
-            cell2Column = num_hash(column - 1)
-            cell3Column = num_hash(column + 1)
-            cell4Column = num_hash(column + 2)
-            searchTarget = "{}{}".format(cell1Column, row)
-            left_cell = "{}{}".format(cell2Column, row)
-            right_cell = "{}{}".format(cell3Column, row)
-            right_2Cells = "{}{}".format(cell4Column, row)
-
-            if (currentSheet[searchTarget].value == Card and cardNameRegex == None): #Can't manipulate the cell value so can't use upper(), title() etc...
-                print(f"There are {currentSheet[right_cell].value} {currentSheet[searchTarget].value} {currentSheet[left_cell].value}. It is at cell {searchTarget}")
-
-            if (currentSheet[searchTarget].value == Card and cardNameRegex != None): #re.match returns None if no matches are found
-                print(f"There are {currentSheet[right_2Cells].value} {currentSheet[searchTarget].value} {currentSheet[right_cell].value}. It is at cell {searchTarget}")
