b20ff7d45317af673df063f64c581d2989da55ba
TC_windows <88120195+TcPirate1@users.noreply.github.com>
2023-02-28 10:01:57 +1300

    Changed back to FFTCG file while editing functions are not finished

--- path.py
+++ path.py
@@ -1,6 +1,6 @@
 import openpyxl
 import os
 
-Path = os.path.join(os.path.expanduser('~'), 'Documents', 'Spreadsheets', 'FFTCG_automated.xlsx')
-File = openpyxl.load_workbook(Path, data_only=False)
+Path = os.path.join(os.path.expanduser('~'), 'Documents', 'Spreadsheets', 'FFTCG')
+File = openpyxl.load_workbook(Path, data_only=True)
 SheetNames = File.sheetnames
