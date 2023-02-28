a46393c041ba11049f9de0cd9ff421278715cb1b
TC_windows <88120195+TcPirate1@users.noreply.github.com>
2023-02-28 21:59:23 +1300

    Accidently deleted extension from Path variable. Fixed this.

--- path.py
+++ path.py
@@ -1,6 +1,6 @@
 import openpyxl
 import os
 
-Path = os.path.join(os.path.expanduser('~'), 'Documents', 'Spreadsheets', 'FFTCG')
+Path = os.path.join(os.path.expanduser('~'), 'Documents', 'Spreadsheets', 'FFTCG.xlsx')
 File = openpyxl.load_workbook(Path, data_only=True)
 SheetNames = File.sheetnames
