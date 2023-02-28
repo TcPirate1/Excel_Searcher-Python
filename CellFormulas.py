ec3758dac7101a6d807987fc9c62eeb034eca765
TC_windows <88120195+TcPirate1@users.noreply.github.com>
2023-02-28 22:05:09 +1300

    Removed unneeded functions, seperated these into a new git branch

--- CellFormulas.py
+++ CellFormulas.py
@@ -1,5 +1,4 @@
 from ColumnConverter import num_hash
-from path import File, Path
 import re
 
 def find_cardLocation(currentSheet, Card):
@@ -21,27 +20,3 @@ def find_cardLocation(currentSheet, Card):
 
             if (currentSheet[searchTarget].value == Card and cardNameRegex != None): #re.match returns None if no matches are found
                 print(f"There are {currentSheet[right_2Cells].value} {currentSheet[searchTarget].value} {currentSheet[right_cell].value}. It is in the {currentSheet[pile].value} pile")
-
-def emptyCells(ActiveWorksheet):
-    print("Checking for empty cells in this sheet...\n")
-    for row in range(1, ActiveWorksheet.max_row + 1):
-        for column in range(1, ActiveWorksheet.max_column + 1):
-            columnLetter = num_hash(column)
-            top = f"{columnLetter}1"
-            searchTarget = f"{columnLetter}{row}"
-            if (ActiveWorksheet[searchTarget].value is None and ActiveWorksheet[top].value == "Code"):
-                print(searchTarget)
-
-def fillEmptyCell(ActiveWorksheet):
-    print("Reminder, empty cells are under the \"Code\" column!")
-    selected_cell = input("Choose the cell you want to write to:\n").upper()
-    selected_cellRight1 = ActiveWorksheet[selected_cell].offset(row= 0, column = 1).coordinate
-    selected_cellRight2 = ActiveWorksheet[selected_cell].offset(row= 0, column = 2).coordinate
-    change_value = input(f"Enter what you want to put in cell {selected_cell}:\n").upper()
-    change_valueRight1 = input(f"Enter what you want to put in this cell {selected_cellRight1}:\n").upper()
-    change_valueRight2 = input(f"Enter what you want to put in this cell {selected_cellRight2}:\n").upper()
-    ActiveWorksheet[selected_cell].value = change_value
-    ActiveWorksheet[selected_cellRight1].value = change_valueRight1
-    ActiveWorksheet[selected_cellRight2].value = change_valueRight2
-    print(f"{ActiveWorksheet[selected_cell].value}, {ActiveWorksheet[selected_cellRight1].value}, {ActiveWorksheet[selected_cellRight2].value}")
-    File.save(Path)
--- Search_fftcg.py
+++ Search_fftcg.py
@@ -5,57 +5,12 @@ from path import File, Path
 def inputChoice():
     userContinue = True
     while userContinue == True:
-        option = input("\nPress one of the following keys to execute their respective functions.\n\nOptions:\n\"E\" to exit the program.\n\"S\" to search for a card.\n\"A\" to add a new worksheet to the workbook.\n\"F\" to fill an empty cell on a worksheet.\n\"V\" to view current selected worksheet.\n\n\"W\" and \"V\" will show empty cells in the \"Code\" column.\n\"C\" to change the value of a cell.\n\n").upper()
+        option = input("\nPress one of the following keys to execute their respective functions.\n\nOptions:\n\"E\" to exit the program.\n\"S\" to search for a card.\n\n").upper()
         if (option == "E"):
             print("The program will now close.\nThank you for using this Python script!")
             break
         if (option == "S"):
             cardFinder()
-        if (option == "A"):
-            new_worksheet = input("What do you want to name this worksheet?\n\nNote: If you don't enter anything it will default to sheet + number.\n").capitalize()
-            File.create_sheet(f"{new_worksheet}")
-            File.save(Path)
-            print("Worksheet created.")
-        if (option == "V"):
-            currentWorksheet = File.active
-            NoCellValue = input(f"Current sheet is: {currentWorksheet}\n\nWould you like to see the empty cells in {currentWorksheet}? (Y = yes, N = no)\n").upper()
-            if (NoCellValue == "Y"):
-                emptyCells(currentWorksheet)
-        if (option == "F"):
-            print(f"Current sheet is: {File.active}")
-            ChangeActiveWorksheet = input("Would you like to change the spreadsheet to work on? (Y = yes, N = no)\n").upper()
-            match ChangeActiveWorksheet:
-                case "Y":
-                    ActiveWorksheet = input("Which sheet would you like to work on?\n").capitalize()
-                    changeSheet(ActiveWorksheet)
-
-            ActiveWorksheet = File.active #Needs to be here so that the variable stays as a worksheet object and not change to a str when we change worksheets.
-            emptyCells(ActiveWorksheet)
-            fillEmptyCell(ActiveWorksheet)
-            
-        if (option == "C"):
-            ActiveWorksheet = File.active
-            ChangeActiveWorksheet = input(f"You are on {ActiveWorksheet}.\nWould you like to change your worksheet? (Y = yes, N = no)\n").upper()
-            match ChangeActiveWorksheet:
-                case "Y":
-                    ActiveWorksheet = input("What sheet do you want to change to?\n").capitalize()
-                    changeSheet(ActiveWorksheet)
-
-            CellOption = input("Do you want to delete a cell? (Y = yes, N = no)\n").upper()
-            match CellOption:
-                case "Y":
-                    CellValue = input("Enter the cell you want to delete the value of: ")
-                    Confirmation = input(f"Are you sure you want to delete {ActiveWorksheet[CellValue].value}? (Y = yes, N = no)\n").upper()
-                    if (Confirmation == "Y"):
-                        ActiveWorksheet[CellValue].value = None
-                        File.save(Path)
-                case "N":
-                    CellValue = input("Enter the cell you want to change the value of: ")
-                    print(f"Value at this cell is: {ActiveWorksheet[CellValue].value}")
-                    ChangeCellValue = input("Enter what you want to put in this cell: ")
-                    ActiveWorksheet[CellValue].value = ChangeCellValue
-                    print(f"{ActiveWorksheet[CellValue]} has been changed to {ChangeCellValue}")
-                    File.save(Path)
 
 def changeSheet(ActiveWorksheet):
     File.active = File[ActiveWorksheet]
